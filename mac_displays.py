4e80a96f3c3e133ff2e0be91dde8569c029540ea
warwick_cdl <matthewsw@c-datalab.com>
2024-06-12 08:47:23 +1000

    refactored to use params file

--- mac_displays.py
+++ mac_displays.py
@@ -1,48 +1,82 @@
 #!/usr/bin/env python
-from __future__ import print_function
+
+# Displayplacer
+# https://github.com/jakehilborn/displayplacer
+
+# macos-wallpaper
+# https://github.com/sindresorhus/macos-wallpaper
+
+# Note: both installed via HomeBbrew
+
+import json
+import sys
 import os
-import pprint
 
-# Hoping these are persistent!
-APPLE_SERIAL_ID = "s4251086178"
-LG_SERIAL_ID = "s16843009"
-PHILIPS_SERIAL_ID = "s5929"
-APPLE_WIDTH = "1728"
-# Define wallpaper paths
+# Determine the path to the script to construct the default params file path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+default_params_path = os.path.join(script_dir, "mac_displays_params.json")
 
-wallpapers = {
-    "apple": "/Users/wazza/Pictures/Dynamic Wallpapers/earth.heic",
-    "philips": "/Users/wazza/Pictures/Dynamic Wallpapers/Star Trek Strange New Worlds V4.heic",
-    "lg": "/Users/wazza/Pictures/Dynamic Wallpapers/Fuji.heic"
-}
+# Use the first command line argument as the params file path if provided, else use the default
+params_file_path = sys.argv[1] if len(sys.argv) > 1 else default_params_path
+
+# Load params from the specified JSON file
+with open(params_file_path, 'r') as file:
+    params = json.load(file)
+    
+
+# params = {
+#     "apple":{
+#         "id":0,
+#         "Description" : "MacBook built in screen",
+#         "Serial screen id" : "s4251086178",
+#         "Wallpaper" : "/Users/wazza/Pictures/Dynamic Wallpapers/earth.heic",
+#         "Width": "1728",
+#     },
+#     "lg":{
+#         "id":1,
+#         "Description" : "LG UltraFine",
+#         "Serial screen id" : "s16843009",
+#         "Wallpaper" : "/Users/wazza/Pictures/Dynamic Wallpapers/Fuji.heic"
+#     },
+#     "philips":{
+#         "id":2,
+#         "Description" : "Philips 328B",
+#         "Serial screen id" : "s5929",
+#         "Wallpaper" : "/Users/wazza/Pictures/Dynamic Wallpapers/Star Trek Strange New Worlds V4.heic"
+#     }
+#     }
+
 
 def getres(res):
+    """
+    Converts a resolution string in the format 'widthxheight' into a tuple of integers (width, height).
+
+    Parameters:
+    - res (str): A string representing the resolution, formatted as 'widthxheight'.
+
+    Returns:
+    - tuple: A tuple of two integers, representing the width and height of the resolution.
+    """
     return tuple(map(int, res.split('x')))
-
-# def load_screen_info(stream):
-
-#     #sections = stream.split("Persistent screen id: ")[1:]  # Skip the first split before the first id
-#     sections = ["Persistent screen id: " + section for section in stream.split("Persistent screen id: ")[1:]]
-#     screen_info_list = []
     
-#     for section in sections:
-#         # Check if "Resolutions for rotation 0:" is in the section and split it off
-#         if "Resolutions for rotation 0:" in section:
-#             section = section.split("Resolutions for rotation 0:")[0]
-        
-#         lines = section.strip().split("\n")
-#         screen_dict = {}
-#         for line in lines:
-#             if ": " in line:  # Ensure there's a key-value pair to split
-#                 key, value = line.split(": ", 1)  # Split on the first occurrence of ": "
-#                 screen_dict[key.strip()] = value.strip()
-#                 if key.strip() == "Enabled" and value.strip() == "true":
-#                     break  # Stop adding more key-value pairs once "Enabled: true" is encountered
-        
-#         screen_info_list.append(screen_dict)
-    
-#     return screen_info_list
+# return screen_info_list
 def load_screen_info(stream):
+
+    """
+    Parses the output from `displayplacer list` command to extract screen information.
+
+    The function splits the input stream by "Persistent screen id: " to identify each screen section,
+    then further processes each section to extract key-value pairs of screen attributes.
+    It ignores lines without a colon separator to filter out irrelevant information.
+    The result is a list of dictionaries, each representing a screen's attributes.
+
+    Parameters:
+    - stream (str): The output string from `displayplacer list` command.
+
+    Returns:
+    - list: A list of dictionaries, where each dictionary contains the attributes of a screen.
+    """
+    
     sections = ["Persistent screen id: " + section for section in stream.split("Persistent screen id: ")[1:]]
     return [
         {key.strip(): value.strip() for key, value in 
@@ -55,8 +89,8 @@ screen_info = load_screen_info(output)
 
 for screen in screen_info:
     screen["Width"], screen["Height"] = getres(screen["Resolution"])
-    if screen["Serial screen id"] == APPLE_SERIAL_ID:
-        APPLE_WIDTH = screen["Width"]
+    if screen["Serial screen id"] == params["apple"]["Serial screen id"]:
+        params['apple']['Width'] = screen["Width"]
 
 apple_wallpaper_id = 0
 LG_monitor_id = 1
@@ -66,15 +100,15 @@ displayplacer_command = "displayplacer "
 
 for i,val in enumerate(screen_info):
     id = val["Serial screen id"]
-    if id == APPLE_SERIAL_ID: 
-        apple_wallpaper_id = i
+    if id == params['apple']['Serial screen id']: 
+        params['apple']['id'] = i
         origin = f"origin:(0,0)"
-    elif id == LG_SERIAL_ID:
-        LG_monitor_id = i
-        origin = f"origin:({int(APPLE_WIDTH/2)},-{val['Height']})"
-    elif id == PHILIPS_SERIAL_ID:
-        philips_monitor_id = i
-        origin = f"origin:(-{int(val['Width']-APPLE_WIDTH/2)},-{val['Height']})"
+    elif id == params["lg"]["Serial screen id"]:
+        params['lg']['id'] = i
+        origin = f"origin:({int(params['apple']['Width']/2)},-{val['Height']})"
+    elif id == params['philips']['Serial screen id']:
+        params['philips']['id'] = i
+        origin = f"origin:(-{int(val['Width']-int(params['apple']['Width'])/2)},-{val['Height']})"
     displayplacer_command += f' \"id:{val.get("Persistent screen id")} res:{val.get("Resolution")} hz:{val.get("Hertz","8")} color_depth:{val.get("Color Depth")} enabled:true scaling:{val.get("Scaling","off")} {origin} degree:0\"'
 
 """
@@ -86,13 +120,11 @@ displayplacer
 
 stream = os.popen(displayplacer_command)
 
-
-
 # Store wallpaper commands in a list
 wallpaper_commands = [
-    f'wallpaper set "{wallpapers["apple"]}" --screen {apple_wallpaper_id}',
-    f'wallpaper set "{wallpapers["philips"]}" --screen {philips_monitor_id}',
-    f'wallpaper set "{wallpapers["lg"]}" --screen {LG_monitor_id}'
+    f'wallpaper set "{params["apple"]["Wallpaper"]}" --screen {params["apple"]["id"]}',
+    f'wallpaper set "{params["philips"]["Wallpaper"]}" --screen {params["philips"]["id"]}',
+    f'wallpaper set "{params["lg"]["Wallpaper"]}" --screen {params["lg"]["id"]}'
 ]
 # Execute each wallpaper command
 for cmd in wallpaper_commands:
